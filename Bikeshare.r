3ee8d6378c83cd905f9283e4e9248bea5d4f562a
Zhuangfang Yi <geospatialanalystyi@gmail.com>
2017-04-30 19:49:17 -0700

    Update Bikeshare.r

--- Bikeshare.r
+++ Bikeshare.r
@@ -1,23 +1,55 @@
 setwd("...")
 list.files("...")
-df <- read.csv("bikeshare.csv", sep = ",")
+
+bike = data.table::fread("train.csv")
+test = data.table::fread("test.csv")
+
+df <- rbind(bike, test)
+
 head(df, 4)
 
-library(ggplot2)
-library(ggthemes)
-library(dplyr)
-library(corrgram)
-library(plotly)
+In [1]:
+library(dplyr, quietly = T, warn.conflicts = F)
+library(data.table, quietly = T, warn.conflicts = F)
+library(fasttime, quietly = T, warn.conflicts = F)
+library(lubridate, quietly = T, warn.conflicts = F)
+library(xgboost, quietly = T, warn.conflicts = F)
+library(ggplot2, quietly = T, warn.conflicts = F)
+library(ggthemes, quietly = T, warn.conflicts = F)
+library(corrgram, quietly = T, warn.conflicts = F)
+library(plotly, quietly = T, warn.conflicts = F)
 Sys.setenv("plotly_username"="...")
 Sys.setenv("plotly_api_key"="...")
 Sys.setenv("plotly_domain"="...")
 
-#check out the correlation between variables
-#Bike.Num <- sapply(df, is.numeric)
-#Bike.cor <- cor(df[,Bike.Num])
-#corrplot::corrplot(Bike.cor, method = "color")
+
+bike$count = log1p(bike$count)
+
+
+bike %>% 
+    mutate(datetime = fastPOSIXct(datetime, "GMT")) %>% 
+    mutate(hour = hour(datetime),
+           month = month(datetime),
+           year = year(datetime),
+           wday = wday(datetime)) -> bike
+
+test %>% 
+    mutate(datetime = fastPOSIXct(datetime, "GMT")) %>% 
+    mutate(hour = hour(datetime),
+           month = month(datetime),
+           year = year(datetime),
+           wday = wday(datetime)) -> test
+
+X_train = bike %>% select(-count, - datetime, -registered, - casual) %>% as.matrix()
+y_train = bike$count
+
+dtrain = xgb.DMatrix(X_train, label = y_train)
+model = xgb.train(data = dtrain, nround = 150, max_depth = 5, eta = 0.1, subsample = 0.9)
+
+xgb.importance(feature_names = colnames(X_train), model) %>% xgb.plot.importance()
+
+
 p1 <- corrgram(df, order = TRUE, lower.panel = panel.shade, upper.panel = panel.pie, text.panel = panel.txt)
-p1
 
 ggplot(df, aes(x=temp, y=count)) + geom_point(aes(colour = temp),alpha=0.5) 
 
@@ -31,7 +63,6 @@ col.tem.count
 ggplot(df, aes(x=factor(season), y=count)) + geom_boxplot(aes(colour=factor(season)))
 
 df$hour <- format(df$datetime,"%H")
-head(df)
 
 workday.bike <- df %>%
   filter(workingday == 1)
@@ -61,4 +92,3 @@ summary(bike.model)
 
 bike.test <- data.frame(season=3, holiday =1, workingday = 0, weather = 1, temp=30, atemp = 42, humidity = 10, windspeed = 2, hour =16, casual =0, registered = 689)
 predict(bike.model, bike.test)
-
